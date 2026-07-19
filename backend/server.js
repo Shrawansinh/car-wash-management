d8083bc3d6bdfa872165a4c9b79f82660701b61a
Shrawansinh prankada <147142619+Shrawansinh@users.noreply.github.com>
2026-07-20 00:23:31 +0530

    Setup backend foundation

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import dotenv from 'dotenv'; // read the env file
+import express from 'express';
+
+const app = express();
+dotenv.config();
+// middleware
+app.use(cors());
+app.use(express.json()); // it is used to read the json onjec which is come form the userSelect: 
+app.get("/",(req,res)=>{
+    res.send("Car wash Api running");
+});
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+});
