f1f6ffd9f0ed5f72ed1c584b5b800fd5ae65152d
Shrawansinh prankada <147142619+Shrawansinh@users.noreply.github.com>
2026-07-21 14:39:25 +0530

    Routes connection is also done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import cors from 'cors';
 import dotenv from 'dotenv'; // read the env file
 import express from 'express';
 import connectDb from './config/db.js';
+import adminRoutes from './routes/adminRoutes.js';
+import bookingRoutes from './routes/bookingRoutes.js';
+import customerRoutes from './routes/customerRoutes.js';
+import galleryRoutes from './routes/galleryRoutes.js';
 
 const app = express();
 dotenv.config();
@@ -12,6 +16,14 @@ app.use(express.json()); // it is used to read the json onjec which is come form
 app.get("/",(req,res)=>{
     res.send("Car wash Api running");
 });
+
+// all routes connection 
+
+app.use("/api/customers",customerRoutes);
+app.use("/api/bookings",bookingRoutes);
+app.use("/api/timeslots",timeSlotROutes);
+app.use("/api/admin",adminRoutes);
+app.use("/api/gallary",galleryRoutes);
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
